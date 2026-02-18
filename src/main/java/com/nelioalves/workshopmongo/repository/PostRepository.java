b9aa885a14f7b75de02461d0c6f20f0bfb58106c
Thiago Carvalho <thiagoaccarv@gmail.com>
2026-02-17 21:00:56 -0300

    Simple query using query methods

--- src/main/java/com/nelioalves/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/nelioalves/workshopmongo/repository/PostRepository.java
@@ -1,9 +1,16 @@
 package com.nelioalves.workshopmongo.repository;
 
 import com.nelioalves.workshopmongo.domain.Post;
+import org.springframework.data.domain.Limit;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
+
+    List<Post> findByTitleContainingIgnoreCase(String text);
+
+
 }
